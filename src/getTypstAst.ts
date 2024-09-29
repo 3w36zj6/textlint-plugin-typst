1c136196a49c09ad201b9a764fbe338d8dae44e2
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-09-29 21:00:22 +0900

    feat: add function to convert raw Typst AST string to object

--- src/getTypstAst.ts
+++ src/getTypstAst.ts
@@ -1,4 +1,5 @@
 import { createTypstCompiler } from "@myriaddreamin/typst.ts/dist/esm/compiler.mjs";
+import { parse } from "yaml";
 
 /**
  * Get the raw Typst AST string from a source string.
@@ -13,4 +14,36 @@ export const getRawTypstAstString = async (source: string) => {
 	const rawTypstAstString = await compiler.getAst("/main.typ");
 
 	return rawTypstAstString;
+};
+
+/**
+ * Convert a raw Typst AST string to a Typst AST object.
+ * @param rawTypstAstString The raw Typst AST string.
+ * @returns The Typst AST object.
+ */
+export const convertRawTypstAstStringToObject = (rawTypstAstString: string) => {
+	const removeFirstLine = (input: string): string => {
+		const lines = input.split("\n");
+		lines.shift();
+		return lines.join("\n");
+	};
+
+	const escapeYamlValues = (yamlString: string): string => {
+		return yamlString
+			.split("\n")
+			.map((line) => {
+				const [key, ...rest] = line.split(":");
+				if (rest[0] === "") return line;
+				const value = rest.join(":").trim();
+				return `${key}: "${value}"`;
+			})
+			.join("\n");
+	};
+
+	const escapedRawTypstAstYamlString = escapeYamlValues(
+		removeFirstLine(rawTypstAstString),
+	);
+
+	const parsed = parse(escapedRawTypstAstYamlString);
+	return parsed.ast;
 };
